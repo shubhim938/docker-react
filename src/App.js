a1af8931cc388966f58663f196095dc475d32c92
Shubhi M <shubhim938@gmail.com>
2025-12-11 10:28:56 +0530

    Update Dockerfile and App.js with new configurations and content

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hi There!</p>
+        <p>I was changed on the feature branch, in hospital!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
